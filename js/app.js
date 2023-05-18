b7e2be244aa4060414e0da73f6dcbb628f372d5c
Jennifer Sung <sung.jenni93@gmail.com>
2023-05-18 17:50:13 -0400

    finished app.js

--- js/app.js
+++ js/app.js
@@ -20,20 +20,44 @@ AppState.prototype.instantiateProducts = function () {
 
 AppState.prototype.saveToLocalStorage = function () {
   // TODO: Fill in this instance method to save product data to local storage
+  let stringProduct = JSON.stringify(this.allProducts);
+  localStorage.setItem('myProducts', stringProduct);
 }
+
+// saveToLocalStorage();
 
 AppState.prototype.loadItems = function () {
-
   // TODO: Update this instance method to retrieve data from local storage instead of creating new Products on each page load
+  let retrieveData = localStorage.getItem('myProducts');
+  let parsedData = JSON.parse(retrieveData);
 
-  this.instantiateProducts();
+  if (retrieveData) {
+    for (let i = 0; i < parsedData.length; i++) {
+      if (parsedData[i].name === 'sweep') {
+        let rebuiltSweep = new Product(parsedData[i].name, 'png');
+        rebuiltSweep.timesShown = parsedData[i].timesShown;
+        rebuiltSweep.timesClicked = parsedData[i].timesClicked;
+        this.allProducts.push(rebuiltSweep);
+      } else {
+        let rebuiltProducts = new Product(parsedData[i].name);
+        rebuiltProducts.timesShown = parsedData[i].timesShown;
+        rebuiltProducts.timesClicked = parsedData[i].timesClicked;
+        this.allProducts.push(rebuiltProducts);
+      }
+    }
+  } else {
+    this.instantiateProducts();
+
+  }
 
 }
 
+// loadItems();
 
 function Product(name, fileExtension = 'jpg') {
   this.name = name;
   this.source = `assets/${name}.${fileExtension}`;
   this.timesClicked = 0;
   this.timesShown = 0;
+
 }
--- js/chart.js
+++ js/chart.js
@@ -11,6 +11,37 @@ let canvasElem = document.getElementById('chart')
  *
  */
 function renderChart() {
+  // Instantiate a new AppState
+  let appState = new App();
+
+
+
 }
 
 renderChart();
+
+
+
+
+// notes:
+//contructor Product in app.js
+// app.js second todo (2:13): right there. But what we want you to do is you need to update this very 
+//similar kind of like what you did with your lab. 13 is, say, hey, if there's things in local storage, 
+//let's do that instead of just instantiating new products all the time.  You can't take the happy path out of the woods today for this one.
+
+
+//nothing to do with voting.js
+
+// let state = new AppStates(): instuating
+
+// state = {
+//  allProducts: [];
+//}
+
+//state.loadItems();
+
+
+
+
+
+
